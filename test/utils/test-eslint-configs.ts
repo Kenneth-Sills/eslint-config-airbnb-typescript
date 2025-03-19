3e0ff47f161b4d68a169d7ab3f44d55bd0056659
Charlie Laabs <github@a.charlielaabs.com>
2025-03-19 14:01:50 -0500

    fix: type linting error

--- test/utils/test-eslint-configs.ts
+++ test/utils/test-eslint-configs.ts
@@ -3,14 +3,14 @@ import type { Linter } from 'eslint';
 export const baseConfig: Linter.LegacyConfig = {
   extends: ['eslint-config-airbnb-base', './base.js'],
   parserOptions: {
-    project: './tsconfig.json',
+    project: './tsconfig.test.json',
   },
 };
 
 export const reactConfig: Linter.LegacyConfig = {
   extends: ['eslint-config-airbnb', './index.js'],
   parserOptions: {
-    project: './tsconfig.json',
+    project: './tsconfig.test.json',
   },
   settings: {
     react: {
