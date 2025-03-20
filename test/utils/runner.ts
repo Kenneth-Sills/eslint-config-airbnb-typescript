0a287fd72dd9c3f883d7fe255639adea11b5f0e6
Charlie Laabs <github@a.charlielaabs.com>
2025-03-20 15:44:56 -0500

    refactor: move test tsconfig location

--- test/utils/runner.ts
+++ test/utils/runner.ts
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'node:fs';
+import path from 'node:path';
 import { describe, it, expect } from 'vitest';
 import { ESLint } from 'eslint';
 import outdent from 'outdent';
--- test/utils/test-eslint-configs.ts
+++ test/utils/test-eslint-configs.ts
@@ -1,9 +1,10 @@
 import type { Linter } from 'eslint';
+import path from 'node:path';
 
 export const baseConfig: Linter.LegacyConfig = {
   extends: ['eslint-config-airbnb-base', './base.js'],
   parserOptions: {
-    project: './tsconfig.test.json',
+    project: path.join(__dirname, 'tsconfig.test.json'),
     disallowAutomaticSingleRunInference: true, // Disable TS caching so it detects files generated inside tests
   },
 };
@@ -11,12 +12,12 @@ export const baseConfig: Linter.LegacyConfig = {
 export const reactConfig: Linter.LegacyConfig = {
   extends: ['eslint-config-airbnb', './index.js'],
   parserOptions: {
-    project: './tsconfig.test.json',
+    project: path.join(__dirname, 'tsconfig.test.json'),
     disallowAutomaticSingleRunInference: true, // Disable TS caching so it detects files generated inside tests
   },
   settings: {
     react: {
-      version: '18',
+      version: '18', // Suppress React detect version warning
     },
   },
 };
